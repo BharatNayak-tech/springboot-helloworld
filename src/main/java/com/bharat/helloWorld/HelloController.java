03c9a8df22ca7691343275511b61e394aedebd02
BharatNayak-tech <bharathillur@gmail.com>
2025-07-20 21:04:10 +0530

    Day 3: Added POST API with @RequestBody - Employee creation

--- src/main/java/com/bharat/helloWorld/HelloController.java
+++ src/main/java/com/bharat/helloWorld/HelloController.java
@@ -1,16 +1,30 @@
 package com.bharat.helloWorld;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController
 
 {
+
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello world !!!";
+    }
+
     @GetMapping("/user")
-public User getUser(){
+     public User getUser(){
 
         return new User("Bharat","abc12@gmail.com");
+    }
+@GetMapping("/employee")
+    public Employee getEmployee(){
+        return new Employee("Bharat","abcd@gmail.com");
+    }
+    @PostMapping("/employee")
+    public String createEmployee(@RequestBody Employee emp) {
+        return "Employee created: " + emp.getName() + " | " + emp.getEmail();
     }
 }
